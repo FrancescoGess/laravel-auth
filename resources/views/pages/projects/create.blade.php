5abab89e967f88d8f12fa1a7a35c2b25a739c827
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-03-26 16:33:05 +0100

    Aggiunta view di creazione

---
+++ resources/views/pages/projects/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="container">
+        <h1>
+            Lista Progetti
+        </h1>
+
+        <form method="POST" action="{{ route('dashboard.projects.store') }}">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control" name="title" id="title" />
+            </div>
+
+            <div class="mb-3">
+                <label for="content" class="form-label">Content</label>
+                <textarea class="form-control" name="content" id="content" rows="3"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary ">Crea nuovo Progetto</button>
+
+
+        </form>
+
+    </main>
+@endsection
