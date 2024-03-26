9fb6afac33e5d3d550e268f03cea744c1103196d
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-03-27 00:10:24 +0100

    aggiunto required statico e dinamico

--- resources/views/pages/projects/create.blade.php
+++ resources/views/pages/projects/create.blade.php
@@ -10,7 +10,22 @@
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" name="title" id="title" />
+                <input 
+                type="text" 
+                class="form-control" 
+                name="title" 
+                id="title" 
+                required/>
+
+                @error('title')
+
+                    <div class="alert alert-danger">
+
+                        {{ $message }}
+
+                    </div>
+
+                @enderror
             </div>
 
             <div class="mb-3">
